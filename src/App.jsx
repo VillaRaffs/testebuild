77edfa9b244cbcb79d927c45e5f3f12279229a84
Rafaela Villatore <morango20n1@gmail.com>
2025-10-17 08:44:05 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <h1>lálálá</h1>
+      <img src="https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/396e9/MainBefore.jpg" alt="camaleão" />
     </>
   )
 }
